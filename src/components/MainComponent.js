75f12befa8bb7ef5218014331b7f0627b37ee895
Jessica Del Fierro <jessicabaldric@gmail.com>
2022-04-15 18:09:56 -0400

    Footer Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { CAMPSITES } from '../shared/campsites';
 
 //container component
@@ -28,6 +29,7 @@ class Main extends Component {
         <Header />
         <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
         <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
+        <Footer />
       </div>
     );
   }
