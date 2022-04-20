a672dae64ec6600cf8f04ac4774aa54b963dd2e6
Jessica Del Fierro <jessicabaldric@gmail.com>
2022-04-20 16:38:26 -0400

    redux react-redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,24 +6,22 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { CAMPSITES } from '../shared/campsites';
-import { COMMENTS } from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+//get the state from redux by setting up the mapStateToProps()
+//change everything in the main component that had this.state to this.props
+const mapStateToProps = state => {
+  return {
+    campsites: state.campsites,
+    comments: state.comments,
+    partners: state.partners,
+    promotions: state.promotions
+  }
+}
 
 //container component
-
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES,
-      comments: COMMENTS,
-      partners: PARTNERS,
-      promotions: PROMOTIONS
-    };
-  }
 
   render() {
     //locally scoped component (defined inside of main component, so only accessible from the main component)
@@ -31,9 +29,9 @@ class Main extends Component {
     const HomePage = () => {
       return (
         <Home 
-          campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
-          promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-          partner={this.state.partners.filter(partner => partner.featured)[0]}
+          campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
+          promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+          partner={this.props.partners.filter(partner => partner.featured)[0]}
         />
       );
     }
@@ -42,8 +40,8 @@ class Main extends Component {
     const CampsiteWithId = ({match}) => {
       return (
         <CampsiteInfo 
-          campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-          comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
+          campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+          comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
         />
       );
     }
@@ -53,8 +51,8 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
-          <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
-          <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+          <Route exact path='/aboutus' render={() => <About partners={this.props.partners} />} />
+          <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
           <Route path='/directory/:campsiteId' component={CampsiteWithId} />
           <Route exact path='/contactus' component={Contact} />
           <Redirect to="/home" />
@@ -78,5 +76,6 @@ class Main extends Component {
 //the <Redirect> component acts like a catch-all such as the default statement in a JS switch statement
 //any routing request that comes through will go through this switch component until it finds a matching route. if there are none, it will end up at the redirect component which will send them to whatever path is there (in this case is home).
 
-
-export default Main;
+//allows the main component to take state from the redux store
+//withRouter will allow react router to work with redux
+export default withRouter(connect(mapStateToProps)(Main));
